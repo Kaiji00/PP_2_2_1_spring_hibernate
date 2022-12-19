629117940cb915894977699d0fc0c31d0922fee3
root <stgorbissi@gmail.com>
2022-12-19 19:00:04 +0300

    FirstSolution

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
@@ -1,10 +1,14 @@
 package hiber.dao;
 
 import hiber.model.User;
+import hiber.model.Car;
 
 import java.util.List;
 
 public interface UserDao {
    void add(User user);
+
+   User getUserByCarModelAndSeries(String model, int series);
+
    List<User> listUsers();
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -8,6 +8,7 @@ public class User {
 
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "id")
    private Long id;
 
    @Column(name = "name")
@@ -18,9 +19,20 @@ public class User {
 
    @Column(name = "email")
    private String email;
+   @OneToOne(cascade = CascadeType.ALL)
+   @JoinColumn(name = "car_id")
+   private Car car;
+
+   public Car getCar() {
+      return car;
+   }
+
+   public void setCar(Car car) {
+      this.car = car;
+   }
 
    public User() {}
-   
+
    public User(String firstName, String lastName, String email) {
       this.firstName = firstName;
       this.lastName = lastName;
@@ -58,4 +70,17 @@ public class User {
    public void setEmail(String email) {
       this.email = email;
    }
+
+   @Override
+   public String toString() {
+      return "User{" +
+              "id=" + id +
+              ", firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              ", car=" + car +
+              '}';
+   }
 }
+
+
